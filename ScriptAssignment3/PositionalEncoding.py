b4a0b2bb806e905f1b20bd46e4a8471d4669363b
Andres Graterol <graterol.andres0@gmail.com>
2024-02-26 20:01:26 -0500

    Initial Assignment3 Commit

---
+++ ScriptAssignment3/PositionalEncoding.py
@@ -0,0 +1,45 @@
+import torch
+import math
+
+from torch import nn
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, embed_dim, max_len):
+        '''
+            embed_dim: embedding dimension of the input
+            max_len: maximum length of the input sequence
+        '''
+        super().__init__()
+        
+        # NOTE: possibly add dropout to prevent overfitting
+
+        # Tensor of 0's 
+        pe = torch.zeros(max_len, embed_dim)
+
+        # Position Column 
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+
+        # Divisor -> 10000^(2i/d)
+        divisor = torch.exp(
+            torch.arange(0, embed_dim, 2).float() * (-math.log(torch.tensor(10000.0)) / embed_dim)
+        )
+
+        # Sin calculation for even indices
+        pe[:, 0::2] = torch.sin(position * divisor)
+
+        # Cos calculation for odd indices
+        pe[:, 1::2] = torch.cos(position * divisor)
+
+        # Unsqueeze to add batch dimension
+        # NOTE: If strange behavior occurs, I have seen this used as pe = pe.unsqueeze(0).transpose(0,1) - which swaps the dimensions after unsqueezing
+        pe = pe.unsqueeze(0)
+
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        '''
+            x: input tensor of shape (seq_len, batch_size, embed_dim)
+        '''
+        # Add positional encoding to input embeddings
+        x = x + self.pe[:, :x.size(1)]
+        return x
